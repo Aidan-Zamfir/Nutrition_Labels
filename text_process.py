8c83b9ab84166d4c0c1983ad083a44675e0b9e0e
Aidan-Z <aidanzamfir@gmail.com>
2024-02-14 10:52:58 -0400

    text_procces because done changing for now

--- text_process.py
+++ text_process.py
@@ -11,7 +11,6 @@ class TextData:
         self.ingredient_list = [] #for string_splice() to add to dict
 
 
-
     def get_text(self): #delete later
         """Use simply to check/print text in temp_ingredients"""
 
@@ -36,52 +35,4 @@ class TextData:
 
         with open('ingredient_dictionary.json', 'a') as f:
             json.dump(self.ingredient_dict, f)
-
-
-
-
-
-
-
-
-
-
-
-
-    # NOT GOING TO USE NLP FOR THIS
-
-    # def process_text(self):
-    #     text = self.get_text()
-    #
-    #     tokens = nltk.word_tokenize(text) # --> tokens are type list
-    #
-    #     for i in tokens: # --> i is type str
-    #         if len(i) > 3:
-    #             self.ingredient_list.append(i) #appends each string to a local list (self.ingredients_list)
-    #
-    #     return tokens
-
-    # def tester(self):
-    #     """NOT WORTH HARD-CODING CHEMICAL NAMES IN...
-    #     FIND NLP UNIT WHICH HAS CHEM. DICT"""
-    #
-    #     x = self.ingredient_list
-    #
-    #     for i, item in enumerate(x):
-    #         if item == "ACID" or item == "ACIDS":
-    #             word = f"{x[i-1]}" + " " + "ACID"
-    #             # print(word)
-    #             x[i] = word
-    #             if item == x[i-1]:
-    #                 x.pop(item[i])
-    #
-    #
-    #
-    #
-    #             # print(x[i-1])
-    #     print(x)
-
-
-
-
 
